1dbf549be9760a86ea123b0b9ae733c0a7daa60f
Faisal Kengo <faisal.kengo@gmail.com>
2022-07-13 13:11:14 +0700

    Commit_07132022_1311

--- Phase_0/Week_1/h8dsft_converter.py
+++ Phase_0/Week_1/h8dsft_converter.py
@@ -1,38 +1,47 @@
-tc = 0.0
+tk = 0.0
 
 def ktoc(t_kelvin):
+    "Konversi Kelvin -> Celsius"
     t_celsius = t_kelvin - 273.15
     return t_celsius
 
 def ctok(t_celsius):
+    "Konversi Celsius -> Kelvin"
     t_kelvin = t_celsius + 273.15
     return t_kelvin
 
 def ktof(t_kelvin):
+    "Konversi Kelvin -> Fahrenheit"
     t_fahrenheit = (t_kelvin * (9.0 / 5.0)) + 32.0
     return t_fahrenheit
 
 def ctof(t_celsius):
+    "Konversi Celsius -> Fahrenheit"
     t_fahrenheit = (t_celsius * (9.0 / 5.0)) + 32.0
     return t_fahrenheit
 
-def ftoc(t_fahrenheit):
-    t_celsius = (t_fahrenheit - 32.0) * (5.0 / 9.0)
-    return t_celsius
-
 def ftok(t_fahrenheit):
+    "Konversi Fahrenheit -> Kelvin"
     t_kelvin = (t_fahrenheit - 32.0) * (5.0 / 9.0)
     return t_kelvin
 
-tck = ctok(tc)
-print(f"{tc} C = {tck} K")
-tkc = ktoc(tck)
-print(f"{tck} K = {tkc} C")
-tcf = ctof(tkc)
-print(f"{tkc} C = {tcf} F")
+def ftoc(t_fahrenheit):
+    "Konversi Fahrenheit -> Celsius"
+    t_celsius = (t_fahrenheit - 32.0) * (5.0 / 9.0)
+    return t_celsius
+
+tkc = ktoc(tk)
+print(f"{tk} K = {tkc} C")
+tck = ctok(tkc)
+print(f"{tkc} C = {tck} K")
 tkf = ktof(tck)
 print(f"{tck} K = {tkf} F")
-tfc = ftoc(tcf)
-print(f"{tcf} F = {tfc} C")
+tcf = ctof(tkc)
+print(f"{tkc} C = {tcf} F")
 tfk = ftok(tkf)
 print(f"{tkf} F = {tfk} K")
+tfc = ftoc(tcf)
+print(f"{tcf} F = {tfc} C")
+
+#Saya masih belum kepikiran bagaimana caranya membuat supaya semua nilainya dinamis,
+#mengingat saya masih menetapkan tk sebagai konstanta awal.
